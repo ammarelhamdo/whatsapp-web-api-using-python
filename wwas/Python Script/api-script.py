363ea8d58ff9fe3e97e49c4b97ee9498654f293c
AMMAR ELHAMDO <ammarayh8991@gmail.com>
2022-02-08 17:02:43 +0300

    Format Code

--- wwas/Python Script/api-script.py
+++ wwas/Python Script/api-script.py
@@ -1,9 +1,24 @@
+'''
+        Resources: This script build using this resources
+        Resource 1: https://www.geeksforgeeks.org/whatsapp-using-python/
+        Resource 2: https://www.geeksforgeeks.org/how-to-read-a-json-response-from-a-link-in-python/
+        Resource 3: https://medium.com/@jihargifari/how-to-send-multiple-whatsapp-message-using-python-3f1f19c5976b
+        Resource 4: https://stackoverflow.com/a/37803474/11937243
+
+        How is working: 
+        1) After opening Chrome to read Whatsapp QR, to sign in, and signed successfully, the script call API to check if new messsages in database inserted,
+           when calling API, the server will search in database and return all new messages as JSON type.
+        2) After API returned the data, script will check if have new messages, and if have, then get every message with phone number and send it.
+'''
+
 # import webdriver library
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+
+# import date time library
 from datetime import datetime
 
 # import urllib library
@@ -12,12 +27,13 @@ from urllib.request import urlopen
   
 # import json & time
 import json
+# import time library for sleep code.
 import time
 
 def requestNewMessages():
         # store the URL in url as 
         # parameter for urlopen
-        req = urllib.request.Request('https://YOUR_DOMAIN.com/wwas/api.php?action=requestsMessagesForUser',headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
+        req = urllib.request.Request('https://DOMAIN_NAME.com/wwas/api.php?action=requestsMessagesForUser',headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
         # store the response of URL
         page = urllib.request.urlopen(req)
           
@@ -42,6 +58,19 @@ def sendMessage(driver, wait, phone, message):
         time.sleep(1)
         return
 
+
+def startChrome():
+        # The absolute path to chromedriver in your computer
+        print ('opening: chrome.')
+        driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+
+        # Call Whatsapp Web Site, and wait for 600 seconds as timeout.
+        print ('opening: web.whatsapp.com.')
+        driver.get("https://web.whatsapp.com/")
+        return driver
+
+
+
 def main():
         # Some Print :_)
         print ('-----------------------------------------------------------------')
@@ -51,13 +80,10 @@ def main():
         print ('||| script starting... |||')
 
 
-        # The absolute path to chromedriver in your computer
-        print ('opening: chrome.')
-        driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+        # Start Chrome
+        driver = startChrome()
 
         # Call Whatsapp Web Site, and wait for 600 seconds as timeout.
-        print ('opening: web.whatsapp.com.')
-        driver.get("https://web.whatsapp.com/")
         wait = WebDriverWait(driver, 600)
 
         # XPath for user profile image, we used for detect if user read QR or not.
@@ -76,7 +102,7 @@ def main():
         # Before send any message we need to wait 20 second for any block from whatsapp.
         time.sleep(20)
 
-
+        # Starting tracking...
         while True:
                 time.sleep(5)
                 print ('Requesting new messages...')
