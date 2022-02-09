8de6600a351d057b6ee03d1453ed9c7a6958e3dc
AMMAR ELHAMDO <ammarayh8991@gmail.com>
2022-02-09 17:06:06 +0300

    Update api file for new adding(Images, Files).

--- wwas/api/api.php
+++ wwas/api/api.php
@@ -15,7 +15,7 @@
     }
     else if (isset($_POST['action']) || isset($_GET['action'])) {
         if ($_POST['action'] == "createRequestMessage") {
-            $object->query = "CALL createRequestMessage('1', '{$_POST['phone']}', '{$_POST['message']}');";
+            $object->query = "CALL createRequestMessage('1', '{$_POST['phone']}', '{$_POST['message']}', '{$_POST['type']}');";
             $object->execute();
             //$status = ($object->row_count() == 0);
             $status = true;
@@ -29,6 +29,7 @@
                 $dat = array();
                 $dat['phone'] = $row['phone'];
                 $dat['message'] = $row['message'];
+                $dat['type'] = $row['type'];
                 $data[] = $dat;
             }
             
